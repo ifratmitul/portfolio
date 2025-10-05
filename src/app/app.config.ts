7d437349a1ae4f15fe86f0dbf3e776b4b0d57a45
Ifrat Ikhtear Uddin <ifratmitul@Ifrats-MacBook-Pro.local>
2025-10-05 00:23:09 -0500

    inital commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,15 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes), 
+    provideAnimationsAsync(),
+    providePrimeNG({ /* options */ })]
 };
+
+
